45e99662b1111b3c10e551d0c71e1ed531fc0456
mdoro-13 <mihai.dorobantu.dev@gmail.com>
2022-10-13 23:32:12 +0300

    Set up routing

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { WeatherInfoComponent } from './weather-info/weather-info.component';
@@ -7,8 +8,8 @@ import { WeatherInfoComponent } from './weather-info/weather-info.component';
 const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'weather-info', component: WeatherInfoComponent },
-  { path: '', component: WeatherInfoComponent, pathMatch: 'full' },
+  { path: 'weather-info', component: WeatherInfoComponent, canActivate: [AuthGuard] },
+  { path: '', redirectTo: 'weather-info', pathMatch: 'full' },
 ];
 
 @NgModule({
