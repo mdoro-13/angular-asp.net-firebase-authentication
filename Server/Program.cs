b2d23112be29bb469976d518f1b47b72f8396b9a
mdoro-13 <mihai.dorobantu.dev@gmail.com>
2022-10-29 18:38:16 +0300

    Implement backend JWT token auth with Firebase

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,7 +1,20 @@
+using FirebaseAdmin;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Server.Authentication;
+
 var builder = WebApplication.CreateBuilder(args);
 var config = builder.Configuration;
 
+const string AUTHENTICATION_SCHEME = JwtBearerDefaults.AuthenticationScheme;
+
+
 // Add services to the container.
+builder.Services.AddSingleton(FirebaseApp.Create());
+
+builder.Services
+    .AddAuthentication(AUTHENTICATION_SCHEME)
+    .AddScheme<AuthenticationSchemeOptions, FirebaseAuthenticationHandler>(AUTHENTICATION_SCHEME, o => { });
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
